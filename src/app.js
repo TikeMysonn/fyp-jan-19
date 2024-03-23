15407503f1b743909422930c925a0754edc3fbb1
Keng Fai <85856597+TikeMysonn@users.noreply.github.com>
2024-03-23 16:18:47 +0800

    tidy up app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const certRoutes = require("./routes/certRoutes");
-require("dotenv").config();
-
 const app = express();
 
-const bodyParser = require("body-parser"); // Import body-parser
-
-// Increase the maximum header size limit
-app.use(bodyParser.json({ limit: "100mb" }));
-
-// Set maximum allowed size for request headers (in bytes)
-app.set("maxHttpHeaderSize", 8111912); // Adjust the value as neededv
-const port = process.env.PORT || 3000;
+require("dotenv").config();
 
 // Enable CORS for all routes and origins
 app.use(cors());
-
 app.use(express.json()); // For parsing application/json
 
 // Use routes
@@ -36,3 +25,11 @@ mongoose
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// const bodyParser = require("body-parser"); // Import body-parser
+// // Increase the maximum header size limit
+// app.use(bodyParser.json({ limit: "100mb" }));
+
+// // Set maximum allowed size for request headers (in bytes)
+// app.set("maxHttpHeaderSize", 8111912); // Adjust the value as neededv
+// const port = process.env.PORT || 3000;

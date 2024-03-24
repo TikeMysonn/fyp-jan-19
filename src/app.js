438b7e2f05ad73eddabba91dcb93052244936e31
Keng Fai <85856597+TikeMysonn@users.noreply.github.com>
2024-03-24 17:34:51 +0800

    fix bug on registering account

--- src/app.js
+++ src/app.js
@@ -15,6 +15,14 @@ app.use(express.json()); // For parsing application/json
 app.use("/api/users", userRoutes);
 app.use("/api/certs", certRoutes);
 
+const bodyParser = require("body-parser"); // Import body-parser
+// Increase the maximum header size limit
+app.use(bodyParser.json({ limit: "100mb" }));
+
+// Set maximum allowed size for request headers (in bytes)
+app.set("maxHttpHeaderSize", 8111912); // Adjust the value as neededv
+const port = process.env.PORT || 3000;
+
 // Connect to MongoDB
 const MONGO_URI = process.env.MONGO_URI;
 mongoose
@@ -25,11 +33,3 @@ mongoose
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// const bodyParser = require("body-parser"); // Import body-parser
-// // Increase the maximum header size limit
-// app.use(bodyParser.json({ limit: "100mb" }));
-
-// // Set maximum allowed size for request headers (in bytes)
-// app.set("maxHttpHeaderSize", 8111912); // Adjust the value as neededv
-// const port = process.env.PORT || 3000;
